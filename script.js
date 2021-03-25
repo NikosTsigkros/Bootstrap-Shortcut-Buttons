208eace2677a64565e26ed046fa9e15af22a7d70
NikosTsigkros <tsigkrosn@gmail.com>
2021-03-26 01:14:57 +0200

    update & fix errors

---
+++ script.js
@@ -0,0 +1,40 @@
+var x;
+
+function runGoogle() {
+    window.open("https://www.google.gr", "_blank");
+}
+
+function runFacebook() {
+    window.open("https://www.facebook.com", "_blank");
+}
+
+function runYoutube() {
+    window.open("https://www.youtube.com", "_blank");
+}
+
+function runTwitter() {
+    window.open("https://www.twitter.com", "_blank");
+}
+
+function runPinterest() {
+    window.open("https://www.pinterest.com", "_blank");
+}
+
+function runNews() {
+    window.open("https://news.google.gr/", "_blank");
+}
+
+function runTranslate() {
+    window.open("https://translate.google.gr/?hl=el&tab=wT", "_blank");
+}
+
+function runGmail() {
+    window.open(
+        "https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/&hl=el#identifier",
+        "_blank"
+    );
+}
+
+function runWeather() {
+    window.open("http://www.meteo.gr/meteoplus/cf.cfm?city_id=375", "_blank");
+}
